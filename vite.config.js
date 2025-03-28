976be2f3cd7bb666247d27107c4bc183f7cfbb05
RshnMng <155337224+RshnMng@users.noreply.github.com>
2025-03-28 12:33:46 -0400

    added to Vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import {defineConfig} from "vite"
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import copy from "rollup-plugin-copy";
 
 export default defineConfig({
-	plugins: [
-		
-	]
-})
+  plugins: [
+    copy({
+      targets: [
+        {
+          src: resolve(__dirname, "_redirects"),
+          dest: resolve(__dirname, "dist"),
+        },
+      ],
+      hook: "closeBundle",
+    }),
+  ],
+  build: {
+    outDir: "dist", // Ensure this matches your build directory
+  },
+});
