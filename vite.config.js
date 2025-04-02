8e77186a450101f9e2161e7bbfd7107dc95a1bcc
RshnMng <155337224+RshnMng@users.noreply.github.com>
2025-04-02 10:06:21 -0400

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,16 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-import copy from "rollup-plugin-copy";
 
 export default defineConfig({
-  plugins: [
-    copy({
-      targets: [
-        {
-          src: resolve(__dirname, "_redirects"),
-          dest: resolve(__dirname, "dist"),
-        },
-      ],
-      hook: "closeBundle",
-    }),
-  ],
   build: {
-    outDir: "dist", // Ensure this matches your build directory
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        "vocab-builder": resolve(__dirname, "vocab-builder.html"),
+        polyglot: resolve(__dirname, "polyglot.html"),
+        "rent-a-van": resolve(__dirname, "rent-a-van.html"),
+        "stock-picker": resolve(__dirname, "stock-picker.html"),
+      },
+    },
   },
 });
